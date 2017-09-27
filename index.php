40089a8117441190609192334c2d1f15846753b8
kedartoraskar <kedar.toraskar@gmail.com>
2017-09-26 21:22:30 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,222 @@
+<?php
+// Get the PHP helper library from twilio.com/docs/php/install
+require __DIR__ . '/vendor/autoload.php';
+use Twilio\Jwt\AccessToken;
+use Twilio\Jwt\Grants\VideoGrant;
+
+// Required for all Twilio access tokens
+$twilioAccountSid = <YOUR ACCOUNT SID>;
+$twilioApiKey = <YOUR API KEY>;
+$twilioApiSecret = <YOUR API SECRET>;
+
+// A unique identifier for this user
+$identity = "alice";
+// The specific Room we'll allow the user to access
+$roomName = 'DailyStandup';
+
+// Create access token, which we will serialize and send to the client
+$token = new AccessToken($twilioAccountSid, $twilioApiKey, $twilioApiSecret, 3600, $identity);
+
+// Create Video grant
+$videoGrant = new VideoGrant();
+$videoGrant->setRoom($roomName);
+
+// Add grant to token
+$token->addGrant($videoGrant);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Twilio Video - Video Quickstart</title>
+  <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<div id="remote-media"></div>
+<div id="controls">
+  <div id="preview">
+    <p class="instructions">Hello Beautiful</p>
+    <div id="local-media"></div>
+    <button id="button-preview">Preview My Camera</button>
+  </div>
+  <div id="room-controls">
+    <button id="button-join">Join Room</button>
+    <button id="button-leave">Leave Room</button>
+  </div>
+  <div id="log"></div>
+</div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="//media.twiliocdn.com/sdk/js/video/v1/twilio-video.min.js"></script>
+<script type="text/javascript">
+
+var activeRoom;
+var previewTracks;
+var identity;
+var roomName;
+
+// Attach the Tracks to the DOM.
+function attachTracks(tracks, container) {
+  tracks.forEach(function(track) {
+    container.appendChild(track.attach());
+  });
+}
+
+// Attach the Participant's Tracks to the DOM.
+function attachParticipantTracks(participant, container) {
+  var tracks = Array.from(participant.tracks.values());
+  attachTracks(tracks, container);
+}
+
+// Detach the Tracks from the DOM.
+function detachTracks(tracks) {
+  tracks.forEach(function(track) {
+    track.detach().forEach(function(detachedElement) {
+      detachedElement.remove();
+    });
+  });
+}
+
+// Detach the Participant's Tracks from the DOM.
+function detachParticipantTracks(participant) {
+  var tracks = Array.from(participant.tracks.values());
+  detachTracks(tracks);
+}
+
+// When we are about to transition away from this page, disconnect
+// from the room, if joined.
+window.addEventListener('beforeunload', leaveRoomIfJoined);
+
+
+  identity = "Alice";
+  document.getElementById('room-controls').style.display = 'block';
+
+  // Bind button to join Room.
+  document.getElementById('button-join').onclick = function() {
+    roomName = ("<?php echo $roomName; ?>");
+    if (!roomName) {
+      alert('Please enter a room name.');
+      return;
+    }
+
+    log("Joining room '" + roomName + "'...");
+    var connectOptions = {
+      name: roomName,
+      logLevel: 'debug'
+    };
+
+    if (previewTracks) {
+      connectOptions.tracks = previewTracks;
+    }
+
+    // Join the Room with the token from the server and the
+    // LocalParticipant's Tracks.
+    Twilio.Video.connect("<?php echo $token; ?>", connectOptions).then(roomJoined, function(error) {
+      log('Could not connect to Twilio: ' + error.message);
+    });
+  };
+
+  // Bind button to leave Room.
+  document.getElementById('button-leave').onclick = function() {
+    log('Leaving room...');
+    activeRoom.disconnect();
+  };
+
+
+// Successfully connected!
+function roomJoined(room) {
+  window.room = activeRoom = room;
+
+  log("Joined as '" + identity + "'");
+  document.getElementById('button-join').style.display = 'none';
+  document.getElementById('button-leave').style.display = 'inline';
+
+  // Attach LocalParticipant's Tracks, if not already attached.
+  var previewContainer = document.getElementById('local-media');
+  if (!previewContainer.querySelector('video')) {
+    attachParticipantTracks(room.localParticipant, previewContainer);
+  }
+
+  // Attach the Tracks of the Room's Participants.
+  room.participants.forEach(function(participant) {
+    log("Already in Room: '" + participant.identity + "'");
+    var previewContainer = document.getElementById('remote-media');
+    attachParticipantTracks(participant, previewContainer);
+  });
+
+  // When a Participant joins the Room, log the event.
+  room.on('participantConnected', function(participant) {
+    log("Joining: '" + participant.identity + "'");
+  });
+
+  // When a Participant adds a Track, attach it to the DOM.
+  room.on('trackAdded', function(track, participant) {
+    log(participant.identity + " added track: " + track.kind);
+    var previewContainer = document.getElementById('remote-media');
+    attachTracks([track], previewContainer);
+  });
+
+  // When a Participant removes a Track, detach it from the DOM.
+  room.on('trackRemoved', function(track, participant) {
+    log(participant.identity + " removed track: " + track.kind);
+    detachTracks([track]);
+  });
+
+  // When a Participant leaves the Room, detach its Tracks.
+  room.on('participantDisconnected', function(participant) {
+    log("Participant '" + participant.identity + "' left the room");
+    detachParticipantTracks(participant);
+  });
+
+  // Once the LocalParticipant leaves the room, detach the Tracks
+  // of all Participants, including that of the LocalParticipant.
+  room.on('disconnected', function() {
+    log('Left');
+    if (previewTracks) {
+      previewTracks.forEach(function(track) {
+        track.stop();
+      });
+    }
+    detachParticipantTracks(room.localParticipant);
+    room.participants.forEach(detachParticipantTracks);
+    activeRoom = null;
+    document.getElementById('button-join').style.display = 'inline';
+    document.getElementById('button-leave').style.display = 'none';
+  });
+}
+
+// Preview LocalParticipant's Tracks.
+document.getElementById('button-preview').onclick = function() {
+  var localTracksPromise = previewTracks
+    ? Promise.resolve(previewTracks)
+    : Twilio.Video.createLocalTracks();
+
+  localTracksPromise.then(function(tracks) {
+    window.previewTracks = previewTracks = tracks;
+    var previewContainer = document.getElementById('local-media');
+    if (!previewContainer.querySelector('video')) {
+      attachTracks(tracks, previewContainer);
+    }
+  }, function(error) {
+    console.error('Unable to access local media', error);
+    log('Unable to access Camera and Microphone');
+  });
+};
+
+// Activity log.
+function log(message) {
+  var logDiv = document.getElementById('log');
+  logDiv.innerHTML += '<p>&gt;&nbsp;' + message + '</p>';
+  logDiv.scrollTop = logDiv.scrollHeight;
+}
+
+// Leave Room.
+function leaveRoomIfJoined() {
+  if (activeRoom) {
+    activeRoom.disconnect();
+  }
+}
+
+</script>
+</body>
+</html>
